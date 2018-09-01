9062efc3f077a42faabf85f66e6f19dd68967133
anandmore <anandmore1997.am@gmail.com>
2018-09-01 16:20:56 +0530

    v1.0.1

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# technofeed
-
+# technofeed v1.0.1
 A material news reader app.
-
 ## Getting Started
-
 For help getting started with Flutter, view our online
 [documentation](https://flutter.io/).
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:timeago/timeago.dart' as timeago;
+import 'package:url_launcher/url_launcher.dart';
+import 'package:share/share.dart';
 
-void main() => runApp(new MyApp());
+String apiKey = "364d218a3b7547ea86d6aff3cf8f3cbe";
+String _apiUrl =
+    "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=";
+List _provider;
+
+void main() async {
+  _provider = await fetchData(_apiUrl + apiKey);
+  runApp(new MyApp());
+}
+
+Future<List> fetchData(apiUrl) async {
+  http.Response response = await http.get(apiUrl);
+  return (json.decode(response.body)['articles']);
+}
+
+_launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Top Headlines',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Top Headlines'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -43,67 +51,79 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        title: new Text(
+          widget.title,
+          style: new TextStyle(
+            fontFamily: 'Ubuntu',
+            fontStyle: FontStyle.italic,
+          ),
         ),
+        centerTitle: true,
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: new ListView.builder(
+          itemCount: _provider.length,
+          itemBuilder: (BuildContext context, int position) {
+            return new Card(
+              elevation: 1.7,
+              child: new ExpansionTile(
+                title: Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0, 7.0, 0.0, 7.0),
+                  child: new Text(
+                    "${_provider[position]['title']}",
+                    style: new TextStyle(
+                      fontSize: 21.0,
+                      fontFamily: 'Ubuntu',
+                    ),
+                  ),
+                ),
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(11.0, 0.0, 11.0, 0.0),
+                    child:
+                        Image.network("${_provider[position]['urlToImage']}"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(11.0, 7.0, 11.0, 7.0),
+                    child: new Text(
+                      "${_provider[position]['description']}",
+                      style: new TextStyle(
+                        fontSize: 13.0,
+                        fontFamily: 'Ubuntu',
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      new Text(
+                        timeago.format(DateTime.parse(
+                            "${_provider[position]['publishedAt']}")),
+                        style: new TextStyle(fontFamily: 'Ubuntu'),
+                      ),
+                      new MaterialButton(
+                        onPressed: () {
+                          String url = "${_provider[position]['url']}";
+                          _launchURL(url);
+                        },
+                        child: new Icon(Icons.launch),
+                      ),
+                      new MaterialButton(
+                        onPressed: () {
+                          Share.share("${_provider[position]['url']}");
+                        },
+                        child: new Icon(Icons.share),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
